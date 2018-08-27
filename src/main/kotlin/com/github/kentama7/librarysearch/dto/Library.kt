9b561dd9e618d9fafae7562c873804079e39bfa7
Kentama7 <Kentama7@users.noreply.github.com>
2018-08-27 21:36:54 +0900

    fix #5 カンマ位置を修正

--- src/main/kotlin/com/github/kentama7/librarysearch/dto/Library.kt
+++ src/main/kotlin/com/github/kentama7/librarysearch/dto/Library.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Library(@JsonProperty("formal") val name: String
-                   , val address: String
-                   , val tel: String
-                   , @JsonProperty("url_pc") val url: String)
+data class Library(@JsonProperty("formal") val name: String,
+                   val address: String,
+                   val tel: String,
+                   @JsonProperty("url_pc") val url: String)
