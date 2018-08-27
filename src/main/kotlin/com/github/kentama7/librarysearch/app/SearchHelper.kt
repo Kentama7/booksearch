f8bb0ab471b05ad4ba66f729c20361eb0bffff75
Kentama7 <Kentama7@users.noreply.github.com>
2018-08-27 21:58:59 +0900

    fix #7 市区町村の半角対策

--- src/main/kotlin/com/github/kentama7/librarysearch/app/SearchHelper.kt
+++ src/main/kotlin/com/github/kentama7/librarysearch/app/SearchHelper.kt
@@ -2,14 +2,19 @@ package com.github.kentama7.librarysearch.app
 
 import com.github.kentama7.librarysearch.data.Pref
 import com.github.kentama7.librarysearch.dto.SearchCondition
+import com.ibm.icu.text.Transliterator
 import org.springframework.stereotype.Component
 
 @Component
 class SearchHelper {
 
+    private val transliterator = Transliterator.getInstance("Halfwidth-Fullwidth")
+
     fun createSearchCondition(form: SearchForm): SearchCondition {
-        // 改ざんされた場合はエラーとする
+        // 都道府県が未入力の場合はエラーとする
         val pref: Pref = form.pref ?: throw IllegalArgumentException("都道府県が指定されていません")
-        return SearchCondition(pref, form.city, form.name)
+        // 半角 -> 全角 (市区町村には半角はないため)
+        val city: String = transliterator.transliterate(form.city)
+        return SearchCondition(pref, city, form.name)
     }
 }
